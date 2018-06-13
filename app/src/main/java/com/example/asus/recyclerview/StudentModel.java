1eb9a2cd0fb1fac9db70846e3140359b10f7ff8e
TorczerA <coolrohit1996@gmail.com>
2018-06-13 15:26:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/asus/recyclerview/StudentModel.java
@@ -0,0 +1,10 @@
+package com.example.asus.recyclerview;
+
+public class StudentModel {
+
+    public String studentName;
+
+    public StudentModel(String studentName) {
+        this.studentName = studentName;
+    }
+}
